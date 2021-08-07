9967715b1a4dbbae7487ebad8e736196c9c10132
sachinDcoder <raja.sachin15@yahoo.com>
2021-08-07 22:27:33 +0530

    coded dp algo

--- src/main/java/myalgo/dp/wineselling/WineSellingMain.java
+++ src/main/java/myalgo/dp/wineselling/WineSellingMain.java
@@ -23,8 +23,36 @@ public class WineSellingMain {
                 , year * prices[j] + calMaxProfitTopDownDp(prices, i, j - 1, year + 1, dp));
     }
 
+    private static int calMaxProfitBottomUp(int[] prices, int N) {
+        int[][] dp = new int[N][N];
+
+        for(int i = N - 1; i >= 0; i--) {
+            for(int j = 0; j < N; j++) {
+                if(i == j) {
+                    dp[i][i] = prices[i] * N;
+                } else if(i < j) {
+                    int y = N - (j - i);
+                    int pickLeft = prices[i] * y + dp[i + 1][j];
+                    int pickRight = prices[j] * y + dp[i][j - 1];
+                    dp[i][j] = Integer.max(pickLeft, pickRight);
+                }
+            }
+        }
+
+        for(int i = 0; i < prices.length; i++) {
+            for(int j = 0; j < prices.length; j++) {
+                System.out.print(dp[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        return dp[0][N-1];
+
+    }
+
     public static void main(String[] args) {
         int[] prices = {2, 3, 5, 1, 4};
+//        int[] prices = {2, 5, 3};
 
         System.out.println(calMaxProfit(prices, 0, prices.length - 1, 1));
 
@@ -37,5 +65,8 @@ public class WineSellingMain {
             }
             System.out.println();
         }
+
+        System.out.println();
+        System.out.println(calMaxProfitBottomUp(prices, prices.length));
     }
 }
